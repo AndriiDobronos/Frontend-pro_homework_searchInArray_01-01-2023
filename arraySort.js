d98b0fdc5062900cf4b31e6b519b863f6ead2791
Andrii Dobronos <andriidobronos@gmail.com>
2023-01-18 13:58:29 +0200

    corrected homework

--- arraySort.js
+++ arraySort.js
@@ -1,19 +1,21 @@
+"use strict";
 //Знайти суму та кількість позитивних елементів.
 const arraySearch = [16,-37,54,-4,72,-56,47,4,
     -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47]
 let sumPositive = 0
-let arrayPositive = []
-for (let m = 0;m <= arraySearch.length ;m++) {
+let numPositive = 0
+const arrPositive = []
+for (let m = 0; m <= arraySearch.length ; m++) {
     if (Number(arraySearch[m]) > 0) {
          sumPositive = sumPositive + arraySearch[m]
-         arrayPositive = arrayPositive + arraySearch[m] + ","
+        numPositive++
     }
 }
-let arrPositive = arrayPositive.split(',')
 alert(`Sum positive items : ${sumPositive} \n
- Number positive items :  ${arrPositive.length - 1}`)
+ Number positive items :  ${numPositive}`)
 /****************************************************/
 //    Знайти мінімальний елемент масиву та його порядковий номер.
+
 let minItem = arraySearch[0]
 for (let j = 1; j <= arraySearch.length ; j++) {
     if (minItem > arraySearch[j]) {
@@ -24,7 +26,7 @@ alert(`Minimal item of array : ${minItem}`)
 let numberMinItem
 for (let k = 0 ; k < arraySearch.length ;k++ ) {
     if (arraySearch[k] > minItem) {
-        numberMinItem = k + 2
+        continue
     }
     else {
         numberMinItem = k + 1
@@ -44,7 +46,7 @@ alert(`Maximal item of array : ${maxItem}`)
 let numberMaxItem
 for (let k = 0 ; k < arraySearch.length ;k++ ) {
     if (arraySearch[k] < maxItem) {
-        numberMaxItem = k + 2
+        continue
     }
     else {
         numberMaxItem = k + 1
@@ -54,7 +56,7 @@ for (let k = 0 ; k < arraySearch.length ;k++ ) {
 alert(`Number minimal item : ${numberMaxItem}`)
 /***************************************************/
 //Визначити кількість негативних елементів.
-let arrNegative = []
+const arrNegative = []
 for (let c = 0; c < arraySearch.length ;c++) {
     for (let b = 0; b < arraySearch.length ; b++) {
         if (arraySearch[c] < 0) {
@@ -68,8 +70,9 @@ alert(`Number negative items :  ${arrNegative.length}`)
 /*********************************************************/
 //    Знайти кількість непарних позитивних елементів.
 let numOddItems = 0
-for (let i =0 ; i < (arrPositive.length - 1) ; i++) {
-    if (!Number.isInteger(arrPositive[i] / 2)) {
+for (let i =0 ; i < arraySearch.length ; i++) {
+    if (!(Number.isInteger(arraySearch[i] / 2)) &&
+    arraySearch[i] > 0) {
         numOddItems++
     }
 }
@@ -77,8 +80,9 @@ alert(`Number odd items is : ${numOddItems}`)
 /*******************************************************/
 //    Знайти кількість парних позитивних елементів.
 let numEvenItems = 0
-for (let q =0 ; q < (arrPositive.length -1); q++) {
-    if (Number.isInteger(arrPositive[q] / 2)) {
+for (let q =0 ; q < arraySearch.length ; q++) {
+    if (Number.isInteger(arraySearch[q] / 2) &&
+        arraySearch[q] > 0) {
         numEvenItems++
     }
 }
@@ -86,26 +90,30 @@ alert(`Number  even items is : ${numEvenItems}`)
 /*******************************************************/
 //    Знайти суму парних позитивних елементів.
 let sumEvenItems = 0
-for (let p =0 ; p < (arrPositive.length -1); p++) {
-    if (Number.isInteger(arrPositive[p] / 2)) {
-       sumEvenItems = sumEvenItems + Number(arrPositive[p])
+for (let p =0 ; p < arraySearch.length ; p++) {
+    if (Number.isInteger(arraySearch[p] / 2) &&
+        arraySearch[p] > 0) {
+       sumEvenItems = sumEvenItems + arraySearch[p]
     }
 }
 alert(`Sum  even items is : ${sumEvenItems}`)
 /******************************************************/
 //   Знайти суму непарних позитивних елементів.
 let sumOddItems = 0
-for (let h =0 ; h < (arrPositive.length - 1) ; h++) {
-    if (!Number.isInteger(arrPositive[h] / 2)) {
-        sumOddItems = sumOddItems + Number(arrPositive[h])
+for (let h =0 ; h < arraySearch.length ; h++) {
+    if (!Number.isInteger(arraySearch[h] / 2) &&
+        arraySearch[h] > 0) {
+        sumOddItems = sumOddItems + arraySearch[h]
     }
 }
 alert(`Sum odd items is : ${sumOddItems}`)
 /*******************************************************/
 //    Знайти добуток позитивних елементів.
 let multPositiveItems = 1
-for (let g = 0; g < (arrPositive.length -1); g++) {
-    multPositiveItems = multPositiveItems * Number(arrPositive[g])
+for (let g = 0; g < arraySearch.length; g++) {
+    if( arraySearch[g] > 0) {
+        multPositiveItems = multPositiveItems * arraySearch[g]
+    }
 }
 alert(`Multiplication positive items is : ${multPositiveItems}`)
 /**********************************************************/
@@ -116,7 +124,7 @@ for (let f = 1; f < arraySearch.length ; f++) {
         bigItem = arraySearch[f]
     }
 }
-let newArraySearch = []
+const newArraySearch = []
 for (let r = 0 ; r < arraySearch.length; r++) {
     if (bigItem > arraySearch[r]) {
         newArraySearch[r] = 0
